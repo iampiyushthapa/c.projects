9803239bcdf6654a35304b5ed91164d4fb3eb1f7
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-16 08:50:07 +0530

    Add files via upload

---
+++ DAY1Q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+    //Q1: Write a program to input two numbers and display their sum.
+
+    int a;
+    int b;
+    printf("Enter number a :");
+    scanf("%d",&a);
+    printf("Enter number b :");
+    scanf("%d",&b);
+   int sum = a + b ;
+printf("The sum is : %d",sum);
+return 0;
+}
+
+                   

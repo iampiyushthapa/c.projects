a0ab3ceb6580aa1a1d61f2214972a4ddba9893a9
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-19 21:53:21 +0530

    Add files via upload

---
+++ DAY5Q10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+
+    //Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+    int t;
+printf("Enter  time (in seconds):");
+scanf("%d",&t);
+int hours = t/(60*60);
+int minutes = (t%3600)/60;
+int seconds = t % 60;
+printf(" The time after conversion is  %d : %d : %d",hours,minutes,seconds);
+return 0;
+}
+
+
+                   

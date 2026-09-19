302854aa67d542dd18ac52cdb6e82e37207ccb68
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-19 22:40:24 +0530

    Add files via upload

---
+++ DAY6Q11.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+
+    //Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+
+    int n;
+printf("Enter the number:");
+scanf("%d",&n);
+if (n%2==0){
+    printf("It is an even number");
+} else {
+    printf("It is an odd number");
+}
+return 0;
+}

bea77eb13dca78503c95392d9f1e7d1d3fb1809a
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-20 21:13:22 +0530

    Add files via upload

---
+++ DAY9Q18.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main (){
+
+    /*Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+90-100: Grade A 
+80-89: Grade B 
+70-79: Grade C 
+60-69: Grade D 
+below 60: Grade F.
+*/
+
+    int a;
+printf("Enter the percentage a (0-100) :");
+scanf("%d",&a);
+    
+    if (a>=90 && a<=100){
+        printf("Grade A");}
+    else if (a>=80 && a<=89){
+    printf("Grade B");}
+    else if(a>=70 && a<=79){
+    printf("Grade C");}
+      else if (a>=60 && a<=69){
+    printf("Grade D");}
+    else {
+    printf("Grade F");}
+    
+return 0;
+}

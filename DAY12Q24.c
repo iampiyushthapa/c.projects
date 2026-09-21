bf48b541eb64853bd0de623ac1f1577d15bbb919
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-21 16:44:06 +0530

    Add files via upload

---
+++ DAY12Q24.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main (){
+
+    /*Q24: Write a program to calculate electricity bill based on units consumed with these rates: 
+First 100 units at ₹5/unit 
+Next 100 units at ₹7/unit 
+Next 100 units at ₹10/unit 
+Above at ₹12/unit
+*/
+
+int u;
+int units = u;
+printf("Enter units: ");
+scanf("%d",&u);
+
+if (u >0 && u<=100){
+     int bill =5*u;
+       printf("The bill is : Rs.%d",bill);}
+
+else if (u >100 && u<=200){
+        int bill =(5 * 100) + ((u - 100)*7);
+       printf("The bill is : Rs.%d",bill);} 
+
+else if (u >200 && u<=300){
+        int bill =(5 * 100) + (7 * 100) + ((u - 200)*10);
+       printf("The bill is : Rs.%d",bill);}
+
+else if (u >300 ){
+    int bill =(5 * 100) + (7 * 100) + ((10 * 100) + (u-300)*12);
+       printf("The bill is : Rs.%d",bill);
+       }
+       
+else {
+       printf("Invalid units");
+}
+return 0;
+}

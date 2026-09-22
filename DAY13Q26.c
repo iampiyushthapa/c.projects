cfa8a0784bb6b0da0416ec340f4999f81428fecb
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-22 06:16:45 +0530

    Add files via upload

---
+++ DAY13Q26.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main (){
+
+// Q26: Write a program to print numbers from 1 to n.
+
+int n;
+int number = n;
+int i;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+for (i=1;i<=n;i++){
+       printf("%d ",i);
+}      
+return 0;
+}
+

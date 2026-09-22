9eb83453f365d6270a1703e2fc951ea29fb00c01
Piyush Thapa <mrlonely8663@gmail.com>
2026-09-22 10:20:58 +0530

    Add files via upload

---
+++ DAY19Q38.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+
+//Q38: Write a program to find the sum of digits of a number.
+
+int i ;
+int digit = 0;
+int n;
+int sum = 0;
+
+printf("Enter number n: ");
+scanf("%d",&n);
+
+for(;n>0 ;n=n/10){
+digit = n % 10;
+sum = sum + digit;
+}
+printf("Tsum is : %d",sum);
+return 0;
+}
